8bf058c904e72eeaf1067892282c90d20d1c429b
JC Brand <jc@opkode.com>
2014-12-04 17:04:40 +0100

    Add files for demoing

--- demo/documentation.md
+++
@@ -1,6 +0,0 @@
-## Description
-
-[Masonry](http://masonry.desandro.com/) is a cascading grid layout library.
-
-This pattern makes it easy to use and configure masonry in a website using
-[Patternslib](http://patternslib.com).
---
+++ main.js
@@ -0,0 +1,40 @@
+require.config({
+    baseUrl: 'src',
+    paths: {
+        "doc-ready":            "bower_components/doc-ready",
+        "eventEmitter":         "bower_components/eventEmitter",
+        "eventie":              "bower_components/eventie",
+        "get-size":             "bower_components/get-size",
+        "get-style-property":   "bower_components/get-style-property",
+        "jquery":               "bower_components/jquery/jquery",
+        "logging":              "bower_components/logging/src/logging",
+        "masonry":              "bower_components/masonry/masonry",
+        "matches-selector":     "bower_components/matches-selector",
+        "outlayer":             "bower_components/outlayer",
+        "pat-compat":           "bower_components/patternslib/src/core/compat",
+        "pat-jquery-ext":       "bower_components/patternslib/src/core/jquery-ext",
+        "pat-logger":           "bower_components/patternslib/src/core/logger",
+        "pat-parser":           "bower_components/patternslib/src/core/parser",
+        "pat-registry":         "bower_components/patternslib/src/core/registry",
+        "pat-utils":            "bower_components/patternslib/src/core/utils",
+        "patterns":             "bower_components/patternslib/bundle"
+    },
+
+    "shim": {
+        "logging": { "exports": "logging" },
+    }
+});
+
+require(["pat-registry", "pat-masonry"], function(registry, upload) {
+    window.patterns = registry;
+    // workaround this MSIE bug :
+    // https://dev.plone.org/plone/ticket/10894
+    if ($.browser.msie) {
+        $("#settings").remove();
+    }
+    window.Browser = {};
+    window.Browser.onUploadComplete = function () {};
+    registry.init();
+    return;
+});
+
